8e659b73556116ae1568c5fe9ddf2750e0f06233
rishabh-kusram <rishabhkusramhp26@gmail.com>
2026-01-01 15:32:49 +0530

    added error message

--- src/controllers/books.controllers.js
+++ src/controllers/books.controllers.js
@@ -3,6 +3,16 @@ const { fetchBooks } = require("../services/books.service");
 exports.getBooks = async (req, res) => {
   try {
     const data = await fetchBooks(req.query);
+    
+    // If no books found, return 404 with error message
+    if (data.count === 0) {
+      return res.status(404).json({
+        error: "No books found matching the specified filters",
+        count: 0,
+        books: []
+      });
+    }
+    
     res.json(data);
   } catch (err) {
     res.status(500).json({ error: err.message });
